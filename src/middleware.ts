ad60cf2d92ed412a3abeceb68b11960cccef6cd8
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-18 13:20:56 -0800

    Huh?

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import {getSession} from "@/scripts/session.ts";
+
+export async function middleware(request: NextRequest) {
+    /**
+     * Add middleware logic
+     */
+}
