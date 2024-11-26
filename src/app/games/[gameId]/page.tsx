94253fd78eeb469a66038f28656db9c49a89e4cc
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-26 11:32:50 -0800

    Added: route params

--- src/app/games/[gameId]/page.tsx
+++ src/app/games/[gameId]/page.tsx
@@ -1,10 +1,13 @@
 import Button from "@/app/components/button.tsx";
 import GameplayManager from "@/app/games/[gameId]/GameplayManager.tsx";
 
-export default async function GamePage() {
+export default async function GamePage({params}: {
+    params: Promise<{ gameId: number }>
+}) {
 
     return (
         <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
+            <p>You are in game {(await params).gameId}</p>
             <GameplayManager/>
         </div>
     )
