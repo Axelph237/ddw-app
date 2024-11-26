2761b23505cb78fa2ee2d1b661eb946062e83f5a
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-26 11:22:04 -0800

    Created: gameplay manager element

---
+++ src/app/games/[gameId]/GameplayManager.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+
+export default function GameplayManager() {
+    const router = useRouter();
+
+    return (
+        <p>In game {router.query.slug}</p>
+    )
+}
--- src/app/games/[gameId]/page.tsx
+++ src/app/games/[gameId]/page.tsx
@@ -1,10 +1,11 @@
 import Button from "@/app/components/button.tsx";
+import GameplayManager from "@/app/games/[gameId]/GameplayManager.tsx";
 
 export default async function GamePage() {
 
     return (
         <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
-            Hello World!
+            <GameplayManager/>
         </div>
     )
 }
