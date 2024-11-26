6d7fa79a7abebc3a90e14c856dfca18b8d36b95f
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-26 11:32:56 -0800

    Updated: internals

--- src/app/games/[gameId]/GameplayManager.tsx
+++ src/app/games/[gameId]/GameplayManager.tsx
@@ -3,9 +3,8 @@
 import { useRouter } from 'next/router';
 
 export default function GameplayManager() {
-    const router = useRouter();
 
     return (
-        <p>In game {router.query.slug}</p>
+        <p>Hullo!</p>
     )
 }
