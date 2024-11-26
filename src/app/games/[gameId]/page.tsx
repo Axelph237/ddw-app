882e008756d0788e477a2a31c0d15a3a5fef40db
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-26 11:20:18 -0800

    Created: game route

---
+++ src/app/games/[gameId]/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/app/components/button.tsx";
+
+export default async function GamePage() {
+
+    return (
+        <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
+            Hello World!
+        </div>
+    )
+}
