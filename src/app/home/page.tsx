e7e3c58bbaa399dcb7b972bb4062e7d1ed86be97
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-26 11:16:51 -0800

    Updated: routeJoin to handleJoin and removed routing

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,15 +3,15 @@ import { redirect } from 'next/navigation';
 
 export default async function HomePage() {
 
-    async function routeJoin() { 'use server'
-        redirect('/join')
+    async function handleJoin() { 'use server'
+        console.log('Join button clicked!')
     }
 
     return (
         <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
             <h2 className='font-[family-name:var(--font-geist-sans)] text-2xl'>welcome to</h2>
             <h1 className='mb-3 mt-1 font-[family-name:var(--font-geist-mono)] text-4xl'>DOGGEE-DOG-WORLD</h1>
-            <Button onClick={routeJoin} text='Join Game' className='m-3' />
+            <Button onClick={handleJoin} text='Join Game' className='m-3' />
         </div>
     )
 }
