b81045ed9b8253468fde726c0177994843930331
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 14:35:23 -0800

    Added: getEntrant method

--- src/app/scripts/entrants.ts
+++ src/app/scripts/entrants.ts
@@ -10,7 +10,7 @@ export interface Entrant {
 }
 
 // Creates entrant under user's account
-export async function createEntrant(entrant:{name: string, weapon: string}) {
+export async function createEntrant(entrant: Entrant) {
     try {
         const response = await fetch(ENTRANTS_URL, {
             method: 'POST',
@@ -18,6 +18,28 @@ export async function createEntrant(entrant:{name: string, weapon: string}) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(entrant),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+
+        return json
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getEntrant(entrantId: number) {
+    try {
+        const response = await fetch(ENTRANTS_URL + `/${entrantId}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            }
         });
 
         if (!response.ok) {
