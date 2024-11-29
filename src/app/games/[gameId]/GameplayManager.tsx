6dc0fc17a1822af7c440708e5caac711793f60e2
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 10:47:24 -0800

    Updated: moved CharacterCreation to its own file

--- src/app/games/[gameId]/GameplayManager.tsx
+++ src/app/games/[gameId]/GameplayManager.tsx
@@ -7,8 +7,11 @@ import Limitedinput, {LimitedInputHandle} from "@/app/components/limitedinput.ts
 import {createEntrant} from "@/scripts/entrants.ts";
 import './Betting.css'
 import {Entrant} from "@/scripts/entrants.ts";
+// Page content components
 import Betting from "@/app/games/[gameId]/pagecontents/Betting.tsx";
 import WaitingRoom from "@/app/games/[gameId]/pagecontents/WaitingRoom.tsx";
+import CharacterCreation from "@/app/games/[gameId]/pagecontents/CharacterCreation.tsx";
+import PostMatch from "@/app/games/[gameId]/pagecontents/PostMatch.tsx";
 
 enum GameplayState {
     CharacterCreation, // On join, before game start
@@ -43,62 +46,4 @@ export default function GameplayManager() {
         case GameplayState.PostMatch:
             return <PostMatch/>
     }
-}
-
-/**
- * Page content when user is creating a character
- *
- * @param handleCreate - the function to call to handle character creation
- * @constructor
- */
-const CharacterCreation = ({
-                               handleCreate
-}: {
-    handleCreate: (entrant: Entrant) => void
-}) => {
-    const [errMsg, setErrMsg] = useState('');
-    const nameInputRef = useRef<LimitedInputHandle>(null)
-    const weaponInputRef = useRef<LimitedInputHandle>(null)
-
-    const handleClick = () => {
-        const nameInput = nameInputRef.current
-        const weaponInput = weaponInputRef.current
-
-        if (!nameInput || !weaponInput) return // Return if elements are lost
-
-        console.log('Creating character', nameInput.value, weaponInput.value)
-        console.log('Name valid?:', nameInput.validInput)
-        console.log('Weapon valid?:', weaponInput.validInput)
-
-        if (nameInput.validInput && weaponInput.validInput && nameInput.value.length > 0 && weaponInput.value.length > 0)  {
-            handleCreate({name: nameInput.value, weapon: weaponInput.value});
-        }
-        else if (!nameInput.validInput || !weaponInput.validInput) {
-            setErrMsg('Woahhh slow down! Those are some pretty big pieces of text.')
-        }
-        else {
-            setErrMsg('An empty entrant is no entrant :/')
-        }
-    }
-
-    return (
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className="text-3xl font-[family-name:var(--font-geist-mono)]">Name Your Challenger!</h1>
-
-            <Limitedinput maxChars={50} id='character-name' type='text' placeholder='Character New Name' ref={nameInputRef}/>
-            <Limitedinput maxChars={50} id='character-weapon' type='text' placeholder='Character Weepon' ref={weaponInputRef}/>
-
-            {/* Character Creation Button*/}
-            <Button text={'Create Entrant'} onClick={handleClick}/>
-            <p className='h-0'>{errMsg}</p>
-        </div>
-    )
-}
-
-
-
-const PostMatch = () => {
-    return (
-        <p>PostMatch</p>
-    )
 }
