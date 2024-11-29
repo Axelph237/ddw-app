15484f617a443d272fa826085d98191aa3d0bf25
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 15:14:39 -0800

    Created: fetchWithAuth.ts to handle auth-required requests

---
+++ src/app/scripts/fetchWithAuth.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import {getSession} from "@/scripts/session.ts";
+
+/**
+ * Adds access token to url.
+ *
+ * @param url - the url to fetch
+ * @param init - the RequestInit object containing fetch options
+ */
+export default async function fetchWithAuth(
+    url: string | URL | globalThis.Request,
+    init?: RequestInit
+) {
+    const session = await getSession()
+
+    if (!session) {
+        throw Error('No authenticated session found.')
+    }
+
+    if (init)  {
+        init.headers = {
+            ...init.headers,
+            'Authorization': `Bearer ${session.accessToken}`
+        }
+    }
+
+    return fetch(url, init)
+}
