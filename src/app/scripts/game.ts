447d8efe2461e85e62851993a456d6d5becde318
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 21:10:23 -0800

    Created: game.ts

---
+++ src/app/scripts/game.ts
@@ -0,0 +1,89 @@
+import fetchWithAuth from "@/scripts/fetchWithAuth.ts";
+
+const GAMES_URL = process.env.NEXT_PUBLIC_API_URL + '/games/'
+
+/**
+ * Starts the current game if it is in lobby and the calling user is the admin
+ *
+ * @param gameId - the game to start
+ */
+export async function startGame(gameId: number) { 'use server'
+    try {
+        const response = await fetchWithAuth(GAMES_URL + `${gameId}/start`, {
+            method: 'POST',
+        });
+
+        return await response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+/**
+ * Joins the current game
+ */
+export async function joinGame() {
+    try {
+        const response = await fetchWithAuth(GAMES_URL + 'join', {
+            method: 'POST',
+        });
+
+        return await response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+/**
+ * Leaves the game.
+ *
+ * @param gameId - the game to leave
+ */
+export async function leaveGame(gameId: number) {
+    try {
+        const response = await fetchWithAuth(GAMES_URL + `${gameId}/leave`, {
+            method: 'POST',
+        });
+
+        return await response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+/**
+ * Gets all players in the lobby.
+ *
+ * @param gameId - the game to search
+ */
+export async function getLobbyPlayers(gameId: number) {
+    try {
+        const response = await fetchWithAuth(GAMES_URL + `${gameId}/lobby`, {
+            method: 'GET',
+        });
+
+        return await response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+/**
+ * Gets the currently running game
+ */
+export async function getCurrentGame() {
+    try {
+        const response = await fetchWithAuth(GAMES_URL + 'current', {
+            method: 'GET',
+        });
+
+        return await response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
