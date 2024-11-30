fe4cc03fab96160ab70c8d81dfccb41de8c3b095
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-11-29 20:32:18 -0800

    Updated: test page's character creation

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,13 +3,33 @@ import {createEntrant} from "@/scripts/entrants.ts";
 
 
 export default async function TestPage() { 'use server'
-    const NAME = 'dummy'
-    const WEAPON = 'nothing'
 
     const handleCreate = async () => { 'use server'
-        const data = await createEntrant({name: NAME, weapon: WEAPON})
+        const name = 'Hitler'
+        const weapon = 'dummy'
 
-        console.log(data)
+        const response = await createEntrant({name, weapon})
+
+        if (!response) {
+            return // Early failure if undefined response
+        }
+
+        const body = await response.json()
+
+        if (response.ok) {
+            console.log('Entrant created with returning data:', body)
+        }
+        else if (body.detail.toString().toLowerCase().includes('inappropriate')) {
+            // Inappropriate words were passed to character creation
+            console.log('Why you putting naughty words in there?')
+        }
+        else if (body.detail.toString().toLowerCase().includes('failed to create')) {
+            // User likely already made an entrant
+            console.log('You probably already made something!')
+        }
+        else {
+            console.log('Entrant creation failed with details:', body.detail)
+        }
     }
 
     return (
