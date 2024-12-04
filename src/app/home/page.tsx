43c0ef582e974273141ab1d39e62db9af15d6b6c
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-04 11:37:47 -0800

    home page redirects to latest game

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import Button from "@/app/components/button.tsx";
 import { redirect } from 'next/navigation';
-import {getUserStatus, joinGame} from "@/scripts/game.ts";
+import {getLatestGame, getUserStatus, joinGame} from "@/scripts/game.ts";
 
 export default async function HomePage() {
 
@@ -16,11 +16,18 @@ export default async function HomePage() {
         redirect('/games')
     }
 
+    async function handleLatestResults() { 'use server'
+        const gameId = await getLatestGame()
+
+        redirect(`/games/results/${gameId}`)
+    }
+
     return (
         <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
             <h2 className='font-[family-name:var(--font-geist-sans)] text-2xl'>welcome to</h2>
             <h1 className='mb-3 mt-1 font-[family-name:var(--font-geist-mono)] text-4xl'>DOGGEE-DOG-WORLD</h1>
             <Button onClick={handleJoin} text='Join Game' className='m-3' />
+            <Button onClick={handleLatestResults} text='Latest Game Results' className='m-3' />
         </div>
     )
 }
