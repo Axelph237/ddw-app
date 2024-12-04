a40a47e658ce2185209925192c6f12dcf7e45e21
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-04 11:36:24 -0800

    games results page

---
+++ src/app/games/results/[gameId]/page.tsx
@@ -0,0 +1,21 @@
+import UserLeaderboard from "@/app/games/results/[gameId]/user-leaderboard.tsx";
+import EntrantsLeaderboard from "@/app/games/results/[gameId]/entrants-leaderboard.tsx";
+
+export const dynamic = 'force-dynamic';
+
+export default async function GameResultsPage({ params }: {params: Promise<{ gameId: number }>}) {
+    const gameId = (await params).gameId
+
+    console.log('Game id:', gameId)
+
+    return (
+            <div className='w-screen h-screen flex flex-col items-center justify-center'>
+                <h1 className='text-4xl font-bold font-[family-name:var(--font-geist-mono)]'>Game {gameId} Results</h1>
+                <div
+                    className='font-[family-name:var(--font-geist-mono)] flex flex-row items-start justify-center gap-4'>
+                    <EntrantsLeaderboard params={{gameId: gameId}}/>
+                    <UserLeaderboard params={{gameId: gameId}}/>
+                </div>
+            </div>
+    )
+}
