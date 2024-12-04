fbbdd0cae66255a9cfbcd239e8234064e23f162f
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-04 10:29:23 -0800

    2 states for if user is admin or normal player

--- src/app/games/pagecontents/WaitingRoom.tsx
+++ src/app/games/pagecontents/WaitingRoom.tsx
@@ -1,4 +1,5 @@
 import Button from "@/app/components/button.tsx";
+import Loading from "@/app/games/pagecontents/Loading.tsx";
 
 /**
  * Page content when user is waiting for game to start. Displays 'Start Game' button when the user is the game admin.
@@ -15,7 +16,8 @@ const WaitingRoom = ({
     return (
         <div className='flex flex-col items-center justify-center'>
             {/*<JumpyDog/>*/}
-            <p>Waiting...</p>
+            <p className='text-3xl'>{handleStart ? 'Start the game when ready!' : 'Waiting'}</p>
+            {!handleStart && <Loading/>}
             {handleStart && <Button text={'Start Game'} onClick={handleStart} className='m-6'/>}
         </div>
     )
