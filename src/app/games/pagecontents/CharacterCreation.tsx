3b881e06e4a20490a0aab8cdd43c9a825f6641ff
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-04 10:37:36 -0800

    Character upload now shows loading once uploaded

--- src/app/games/pagecontents/CharacterCreation.tsx
+++ src/app/games/pagecontents/CharacterCreation.tsx
@@ -2,6 +2,7 @@ import {Entrant} from "@/scripts/entrants.ts";
 import {useEffect, useRef, useState} from "react";
 import Limitedinput, {LimitedInputHandle} from "@/app/components/limitedinput.tsx";
 import Button from "@/app/components/button.tsx";
+import Loading from "@/app/games/pagecontents/Loading.tsx";
 
 /**
  * Page content when user is creating a character
@@ -13,13 +14,15 @@ const CharacterCreation = ({
                                handleCreate, creationError
                            }: {
     handleCreate: (entrant: Entrant) => void,
-    creationError: string
+    creationError: string,
 }) => {
+    const [uploaded, setUploaded] = useState(false)
     const [errMsg, setErrMsg] = useState('')
     const nameInputRef = useRef<LimitedInputHandle>(null)
     const weaponInputRef = useRef<LimitedInputHandle>(null)
 
     const handleClick = () => {
+        setUploaded(true)
         const nameInput = nameInputRef.current
         const weaponInput = weaponInputRef.current
 
@@ -34,26 +37,40 @@ const CharacterCreation = ({
         }
         else if (!nameInput.validInput || !weaponInput.validInput) {
             setErrMsg('Woahhh slow down! Those are some pretty big pieces of text.')
+            setUploaded(false)
         }
         else {
             setErrMsg('An empty entrant is no entrant :/')
+            setUploaded(false)
         }
     }
 
     useEffect(() => {
         setErrMsg(creationError)
+        setUploaded(false)
     }, [creationError])
 
     return (
         <div className='flex flex-col justify-center items-center'>
-            <h1 className="text-3xl font-[family-name:var(--font-geist-mono)]">Name Your Challenger!</h1>
+            {uploaded
+                ? <>
+                    <p className='text-3xl font-[family-name:var(--font-geist-mono)]'>Uploading Character</p>
+                    <Loading />
+                </>
+                : <>
+                    <h1 className="text-3xl font-[family-name:var(--font-geist-mono)]">Name Your Challenger!</h1>
 
-            <Limitedinput maxChars={50} id='character-name' type='text' placeholder='Character New Name' ref={nameInputRef}/>
-            <Limitedinput maxChars={50} id='character-weapon' type='text' placeholder='Character Weapon' ref={weaponInputRef}/>
+                    <Limitedinput maxChars={50} id='character-name' type='text' placeholder='Character New Name'
+                                  ref={nameInputRef}/>
+                    <Limitedinput maxChars={50} id='character-weapon' type='text' placeholder='Character Weapon'
+                                  ref={weaponInputRef}/>
 
-            {/* Character Creation Button*/}
-            <Button text={'Create Entrant'} onClick={handleClick}/>
-            <p className='h-0'>{errMsg}</p>
+                    {/* Character Creation Button*/}
+                    <Button text={'Create Entrant'} onClick={handleClick}/>
+                    <p className='h-0'>{errMsg}</p>
+                </>
+
+            }
         </div>
     )
 }
