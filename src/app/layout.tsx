5f4e86f200fc5d9c2bf16bbc8444d09cf1f871bb
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-05 12:02:38 -0800

    Added: .ico variant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export const metadata: Metadata = {
     title: "D0GG33 D0G W0RLD",
     description: "The best online betting game!",
     icons: {
-        icon: '/icon.png',
-        shortcut: '/icon.png',
-        apple: '/icon.png',
+        icon: '/icon.ico',
+        shortcut: '/icon.ico',
+        apple: '/icon.ico',
         other: {
             rel: 'apple-touch-icon',
-            url: '/icon.png',
+            url: '/icon.ico',
         },
     },
 };
