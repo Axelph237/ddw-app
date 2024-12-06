5515352a3d234ad7ec42f5e949d3e247223d9c23
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-06 11:17:08 -0800

    Some formatting thing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
     return (
         <>
             <h1 className='absolute m-6 font-[family-name:var(--font-geist-mono)] text-4xl'>D0GG33-D0G-W0RLD</h1>
-            <StateManager handleData={handleLogin} />
+            <StateManager handleData={handleLogin}/>
         </>
     );
 }
