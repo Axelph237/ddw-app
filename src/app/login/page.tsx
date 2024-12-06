d84fdeeb70e6d3351e775aa0698620db278ee8b8
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-06 12:09:13 -0800

    Removed auto login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,10 +28,6 @@ export default function LoginPage() {
         else if (data.action === "register" && data.username && data.email) {
             const response = await register(data.username, data.email, data.password)
             console.log('Successfully logged in with response:', response)
-
-            if (response) {
-                await handleLogin({username: data.username, password: data.password, action: 'login'})
-            }
         }
     }
 
