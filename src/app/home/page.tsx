1280637bd301bcb589439e4654b072f0f0412f99
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-06 10:08:22 -0800

    Leet speak on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -33,7 +33,7 @@ export default async function HomePage() {
     return (
         <div className='font-[family-name:var(--font-geist-sans)] w-screen h-screen flex flex-col items-center justify-center'>
             <h2 className='font-[family-name:var(--font-geist-sans)] text-2xl'>welcome to</h2>
-            <h1 className='mb-3 mt-1 font-[family-name:var(--font-geist-mono)] text-4xl'>DOGGEE-DOG-WORLD</h1>
+            <h1 className='mb-3 mt-1 font-[family-name:var(--font-geist-mono)] text-4xl'>D0GG33-D0G-W0RLD</h1>
             <Button onClick={handleJoin} text='Join Game' className='m-3' />
             <Button onClick={handleLatestResults} text='Latest Game Results' className='m-3' />
         </div>
