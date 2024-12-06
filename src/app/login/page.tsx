4456c9823b8d054a4c37f71d7f34c292b0f99190
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-06 11:26:57 -0800

    Remformatting and fix to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,6 @@ export default function LoginPage() {
 
         if (data.action === "login" && data.email) {
             const response = await login(data.email, data.password)
-
             console.log('Successfully logged in with response:', response)
 
             if (response) redirect('/home')
@@ -29,6 +28,8 @@ export default function LoginPage() {
         else if (data.action === "register" && data.username && data.email) {
             const response = await register(data.username, data.email, data.password)
             console.log('Successfully logged in with response:', response)
+
+            if (response) redirect('/home')
         }
     }
 
