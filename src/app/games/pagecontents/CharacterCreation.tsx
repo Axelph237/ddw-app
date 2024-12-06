5633a6ec64051a6025dad7b0f4d74a0604aec295
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-12-06 09:58:30 -0800

    Update: to use error alert

--- src/app/games/pagecontents/CharacterCreation.tsx
+++ src/app/games/pagecontents/CharacterCreation.tsx
@@ -1,8 +1,9 @@
 import {Entrant} from "@/scripts/entrants.ts";
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import Limitedinput, {LimitedInputHandle} from "@/app/components/limitedinput.tsx";
 import Button from "@/app/components/button.tsx";
 import Loading from "@/app/games/pagecontents/Loading.tsx";
+import {dispatchError} from "@/app/components/erroralert/erroralert.tsx";
 
 /**
  * Page content when user is creating a character
@@ -11,44 +12,31 @@ import Loading from "@/app/games/pagecontents/Loading.tsx";
  * @constructor
  */
 const CharacterCreation = ({
-                               handleCreate, creationError
+                               handleCreate
                            }: {
-    handleCreate: (entrant: Entrant) => void,
-    creationError: string,
+    handleCreate: (entrant: Entrant, onErr: () => void) => void,
 }) => {
     const [uploaded, setUploaded] = useState(false)
-    const [errMsg, setErrMsg] = useState('')
     const nameInputRef = useRef<LimitedInputHandle>(null)
     const weaponInputRef = useRef<LimitedInputHandle>(null)
 
     const handleClick = () => {
-        setUploaded(true)
         const nameInput = nameInputRef.current
         const weaponInput = weaponInputRef.current
 
         if (!nameInput || !weaponInput) return // Return if elements are lost
 
-        console.log('Creating character', nameInput.value, weaponInput.value)
-        console.log('Name valid?:', nameInput.validInput)
-        console.log('Weapon valid?:', weaponInput.validInput)
-
         if (nameInput.validInput && weaponInput.validInput && nameInput.value.length > 0 && weaponInput.value.length > 0)  {
-            handleCreate({name: nameInput.value, weapon: weaponInput.value});
+            setUploaded(true)
+            handleCreate({name: nameInput.value, weapon: weaponInput.value}, () => setUploaded(false));
         }
         else if (!nameInput.validInput || !weaponInput.validInput) {
-            setErrMsg('Woahhh slow down! Those are some pretty big pieces of text.')
-            setUploaded(false)
+            dispatchError('Woahhh slow down! Those are some pretty big pieces of text.')
         }
         else {
-            setErrMsg('An empty entrant is no entrant :/')
-            setUploaded(false)
+            dispatchError('An empty entrant is no entrant :/')
         }
     }
-
-    useEffect(() => {
-        setErrMsg(creationError)
-        setUploaded(false)
-    }, [creationError])
 
     return (
         <div className='flex flex-col justify-center items-center'>
@@ -67,7 +55,7 @@ const CharacterCreation = ({
 
                     {/* Character Creation Button*/}
                     <Button text={'Create Entrant'} onClick={handleClick}/>
-                    <p className='h-0'>{errMsg}</p>
+                    {/*<p className='h-0'>{errMsg}</p>*/}
                 </>
 
             }
